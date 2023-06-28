9dafb09c1ee4b1c5e6e73b5c777514df44715bb7
493611 <493611@student.saxion.nl>
2023-06-28 19:45:43 +0200

    Add testCanLoadBinAsUser()

--- mod/bin/tests/phpunit/integration/ElggBinIntegrationTest.php
+++ mod/bin/tests/phpunit/integration/ElggBinIntegrationTest.php
@@ -11,20 +11,72 @@ class ElggBinIntegrationTest extends \Elgg\IntegrationTestCase {
     private \ElggUser $user;
     private \ElggGroup $group_by_admin;
     private \ElggGroup $group_by_user;
-    private \ElggBlog $blog_post_1; // owned by admin, soft deleted by admin
-    private \ElggBlog $blog_post_2; // owned by user, soft deleted by admin
-    private \ElggBlog $blog_post_3; // owned by user, soft deleted by user
+    private \ElggObject $blog_post_1; // owned by admin, soft deleted by admin
+    private \ElggObject $blog_post_2; // owned by user, soft deleted by admin
+    private \ElggObject $blog_post_3; // owned by user, soft deleted by user
 
     public function up()
     {
         $this->admin = $this->createUser();
+        $this->admin->makeAdmin();
         $this->user = $this->createUser();
+
         $this->group_by_admin = $this->createGroup();
-        //$this->group_by_admin->owner_guid =
+        $this->group_by_admin->owner_guid = $this->admin->guid;
+        $this->group_by_admin->container_guid = $this->admin->guid;
+
+        $this->group_by_user = $this->createGroup();
+        $this->group_by_user->owner_guid = $this->user->guid;
+        $this->group_by_user->container_guid = $this->user->guid;
+
+        $this->blog_post_1 = $this->createObject([
+            'subtype' => 'blog',
+            'comments_on' => 'On',
+            'status' => 'published',
+            'owner_guid' => $this->admin->guid,
+            'container_guid' => $this->group_by_admin->guid,
+        ]);
+
+        $this->blog_post_2 = $this->createObject([
+            'subtype' => 'blog',
+            'comments_on' => 'On',
+            'status' => 'published',
+            'owner_guid' => $this->user->guid,
+            'container_guid' => $this->group_by_user->guid,
+        ]);
+
+        $this->blog_post_3 = $this->createObject([
+            'subtype' => 'blog',
+            'comments_on' => 'On',
+            'status' => 'published',
+            'owner_guid' => $this->user->guid,
+            'container_guid' => $this->group_by_user->guid,
+        ]);
+
     }
 
     public function testCanLoadBinAsUser() {
+        _elgg_services()->session_manager->setLoggedInUser($this->user);
 
+        // nothing has been soft deleted yet
+        $this->assertEquals(elgg_get_entities([
+            'owner_guid' => $this->user->guid,
+            'relationship' => 'deleted_by',
+            'type_subtype_pairs' => elgg_entity_types_with_capability('soft_deletable'),
+            'inverse_relationship' => false,
+            'no_results' => true
+        ]), []);
+
+        // should only be able to see blog 3?
+        $this->blog_post_2->softDelete($this->admin->guid);
+        $this->blog_post_3->softDelete($this->user->guid);
+        $this->assertEquals(elgg_get_entities([
+            'owner_guid' => $this->user->guid,
+            'relationship' => 'deleted_by',
+            'type_subtype_pairs' => elgg_entity_types_with_capability('soft_deletable'),
+            'inverse_relationship' => false,
+            'no_results' => true
+        ]), [$this->blog_post_3]);
 
     }
 
